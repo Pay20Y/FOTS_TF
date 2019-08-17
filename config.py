44b31ec9770027224821ae57478b29b449c8af5f
qiaozhi <qiaozhi@iie.ac.cn>
2019-08-17 16:03:46 +0800

    clear master branch

--- config.py
+++
@@ -1,3 +0,0 @@
-# coding:utf-8
-CHAR_VECTOR = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-~`<>'.:;^/|!?$%#@&*()[]{}_+=,\\\""
-NUM_CLASSES = len(CHAR_VECTOR) + 1
--- module/stn/__init__.py
+++
@@ -1,1 +0,0 @@
-from .transformer import spatial_transformer_network
